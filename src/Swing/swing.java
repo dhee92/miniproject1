421a48641bb6f50d3d31d4a268aa8d32092d8363
DAHEELEE <daheelee1114@gmail.com>
2018-12-11 16:45:15 +0900

    swibg

--- src/Swing/swing.java
+++ src/Swing/swing.java
@@ -23,10 +23,17 @@ import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
 import java.awt.GridLayout;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JTable;
 
 public class swing {
 	
@@ -132,24 +139,79 @@ public class swing {
 		menu.setBackground(UIManager.getColor("CheckBox.background"));
 		menu.setForeground(Color.BLACK);
 		panel_2.add(menu);
-		menu.setLayout(new GridLayout(2, 4, 0, 0));
+		menu.setLayout(null);
 		
 		//메뉴버튼 8개 이미지 넣을지 말지.... 
-		JButton menu1 = new JButton("Iced Americano");
+		JButton menu1 = new JButton();
+		menu1.setBounds(0, 0, 150, 250);
 		menu.add(menu1);
-		JButton menu2 = new JButton("Iced Latte");
+		menu1.setLayout(new BorderLayout());
+		JLabel menu1Label1 = new JLabel("Iced Americano");
+		JLabel menu1Label2 = new JLabel("\t\t3000");
+		menu1.add(menu1Label1, BorderLayout.NORTH);
+		menu1.add(menu1Label2, BorderLayout.SOUTH);
+		
+		JButton menu2 = new JButton();
+		menu2.setBounds(150, 0, 150, 250);
+		menu2.setLayout(new BorderLayout());
+		JLabel menu2Label1 = new JLabel("Iced Latte");
+		JLabel menu2Label2 = new JLabel("\t\t3000");
+		menu2.add(menu2Label1, BorderLayout.NORTH);
+		menu2.add(menu2Label2, BorderLayout.SOUTH);
 		menu.add(menu2);
-		JButton menu3 = new JButton("Iced Vanilla Latte");
+		
+		JButton menu3 = new JButton();
+		menu3.setBounds(300, 0, 150, 250);
+		menu3.setLayout(new BorderLayout());
+		JLabel menu3Label1 = new JLabel("Iced Vanilla Latte");
+		JLabel menu3Label2 = new JLabel("\t\t3000");
+		menu3.add(menu3Label1, BorderLayout.NORTH);
+		menu3.add(menu3Label2, BorderLayout.SOUTH);
 		menu.add(menu3);
-		JButton menu4 = new JButton("Iced Mocha");
+		
+		JButton menu4 = new JButton();
+		menu4.setBounds(450, 0, 150, 250);
+		menu4.setLayout(new BorderLayout());
+		JLabel menu4Label1 = new JLabel("Iced Mocha");
+		JLabel menu4Label2 = new JLabel("\t\t3000");
+		menu4.add(menu4Label1, BorderLayout.NORTH);
+		menu4.add(menu4Label2, BorderLayout.SOUTH);
 		menu.add(menu4);
-		JButton menu5 = new JButton("Hot Americano");
+		
+		JButton menu5 = new JButton();
+		menu5.setBounds(0, 250, 150, 250);
+		menu5.setLayout(new BorderLayout());
+		JLabel menu5Label1 = new JLabel("Hot Americano");
+		JLabel menu5Label2 = new JLabel("\t\t3000");
+		menu5.add(menu5Label1, BorderLayout.NORTH);
+		menu5.add(menu5Label2, BorderLayout.SOUTH);
 		menu.add(menu5);
-		JButton menu6 = new JButton("Hot Latte");
+		
+		JButton menu6 = new JButton();
+		menu6.setBounds(150, 250, 150, 250);
+		menu6.setLayout(new BorderLayout());
+		JLabel menu6Label1 = new JLabel("Hot Latte");
+		JLabel menu6Label2 = new JLabel("\t\t3000");
+		menu6.add(menu6Label1, BorderLayout.NORTH);
+		menu6.add(menu6Label2, BorderLayout.SOUTH);
 		menu.add(menu6);
-		JButton menu7 = new JButton("Hot Vanilla Latte");
+		
+		JButton menu7 = new JButton();
+		menu7.setBounds(300, 250, 150, 250);
+		menu7.setLayout(new BorderLayout());
+		JLabel menu7Label1 = new JLabel("Hot Vanilla Latte");
+		JLabel menu7Label2 = new JLabel("\t\t3000");
+		menu7.add(menu7Label1, BorderLayout.NORTH);
+		menu7.add(menu7Label2, BorderLayout.SOUTH);
 		menu.add(menu7);
-		JButton menu8 = new JButton("Hot Mocha");
+		
+		JButton menu8 = new JButton();
+		menu8.setBounds(450, 250, 150, 250);
+		menu8.setLayout(new BorderLayout());
+		JLabel menu8Label1 = new JLabel("Hot Mocha");
+		JLabel menu8Label2 = new JLabel("\t\t3000");
+		menu8.add(menu8Label1, BorderLayout.NORTH);
+		menu8.add(menu8Label2, BorderLayout.SOUTH);
 		menu.add(menu8);
 		
 		
@@ -163,12 +225,21 @@ public class swing {
 		salelabel.setBounds(300, 6, 108, 29);
 		sale.add(salelabel);
 		JButton charge = new JButton("Charge : ");
-		charge.setAction(action);
 		charge.setFont(new Font("Arial", Font.BOLD, 15));
 		charge.setForeground(new Color(0, 0, 0));
 		charge.setBounds(0,400,600,100);
 		charge.setBackground(new Color(0, 0, 0));//2896CC 이 컬러코드로 하고싶다. 
 		sale.add(charge);
+		
+		//JTextArea 가운데 정렬 
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(129, 77, 0, 16);
+//		Document document = textArea.getDocument();
+//		SimpleAttributeSet center = new SimpleAttributeSet();
+//		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+//		((StyledDocument) document).setParagraphAttributes(0, document.getLength(), center, false);
+		sale.add(textArea);
+				
 		
 		
 		JPanel bottom = new JPanel();
@@ -203,6 +274,53 @@ public class swing {
 			}
 			
 		});
+		
+		textArea.setVisible(true);
+		menu1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText(menu1Label1.getText());
+				textArea.setText(menu1Label2.getText());
+				
+				
+			}
+		});
+		menu2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText(menu2Label1.getText());
+				textArea.setText(menu2Label2.getText());
+			}
+		});
+		menu3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.replaceSelection(menu3.getText());
+			}
+		});
+		menu4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.replaceSelection(menu4.getText());
+			}
+		});
+		menu5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.replaceSelection(menu5.getText());
+			}
+		});
+		menu6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.replaceSelection(menu6.getText());
+			}
+		});
+		menu7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.replaceSelection(menu7.getText());
+			}
+		});
+		menu8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.replaceSelection(menu8.getText());
+			}
+		});
+		
 		
 
 		 
