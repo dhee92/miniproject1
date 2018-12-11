5e610661587435282f1bc45beffef501f47aee47
DAHEELEE <daheelee1114@gmail.com>
2018-12-11 09:46:39 +0900

    swing

--- src/Swing/swing.java
+++ src/Swing/swing.java
@@ -21,6 +21,12 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.JSplitPane;
+import javax.swing.UIManager;
+import java.awt.GridLayout;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 public class swing {
 	
@@ -40,6 +46,7 @@ public class swing {
 	}
 
 	private JFrame frame;
+	private final Action action = new SwingAction();
 
 	/**
 	 * Launch the application.
@@ -70,7 +77,7 @@ public class swing {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setTitle("Cafe POS");
-		frame.getContentPane().setFont(new Font("NanumGothic", Font.PLAIN, 13));
+		frame.getContentPane().setFont(new Font("Arial", Font.PLAIN, 13));
 		frame.setBounds(100, 100, 1200, 630);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false); 
@@ -78,16 +85,16 @@ public class swing {
 		frame.setResizable(false);
 		frame.getContentPane().setLayout(null);
 		
-		ImagePanel imagePanel = new ImagePanel(new ImageIcon("./image/image_login.jpg").getImage());
+		ImagePanel panel_1 = new ImagePanel(new ImageIcon("./image/image_login.jpg").getImage());
 //		File f = new File("./image/image_login.jpg");
 //		System.out.println(f.exists()?"Exists":"doesn't exist");
-		frame.getContentPane().add(imagePanel);
+		//frame.getContentPane().add(imagePanel);
 		//frame.pack(); 
 	
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(6, 6, 1200, 630);
+		//JPanel panel_1 = new JPanel();
+		//panel_1.setBounds(6, 6, 1200, 630);
 		JLabel id = new JLabel("ID : ");
-		id.setBounds(553, 10, 39, 19);
+		id.setBounds(382, 9, 39, 19);
 		id.setFont(new Font("Arial", Font.BOLD, 16));
 		id.setForeground(Color.WHITE);
 		JLabel pswd = new JLabel("PW : ");
@@ -100,7 +107,8 @@ public class swing {
 		txtpass.setBounds(597, 6, 130, 26);
 		JButton logBtn =  new JButton("Log In");
 		logBtn.setBounds(732, 5, 83, 29);
-		panel_1.setLayout(null);
+		
+		//panel_1.setLayout(null);
 		
 		panel_1.add(id);
 		panel_1.add(txtID);
@@ -108,20 +116,71 @@ public class swing {
 		panel_1.add(txtpass);
 		panel_1.add(logBtn);
 		
-		frame.getContentPane().add(panel_1); 
+		//frame.getContentPane().add(panel_1); 
+		frame.getContentPane().add(panel_1);
 		panel_1.setVisible(true);
 		
-		
+
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(6, 6, 1200, 630);
+		panel_2.setBackground(UIManager.getColor("CheckBox.background"));
+		panel_2.setBounds(0, 0, 1200, 630);
 		frame.getContentPane().add(panel_2);
 		panel_2.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(335, 5, 117, 29);
-		panel_2.add(btnNewButton);
+		JPanel menu = new JPanel();
+		menu.setBounds(0, 30, 600, 500);
+		menu.setBackground(UIManager.getColor("CheckBox.background"));
+		menu.setForeground(Color.BLACK);
+		panel_2.add(menu);
+		menu.setLayout(new GridLayout(2, 4, 0, 0));
 		
-		panel_2.setVisible(false);
+		//메뉴버튼 8개 이미지 넣을지 말지.... 
+		JButton menu1 = new JButton("Iced Americano");
+		menu.add(menu1);
+		JButton menu2 = new JButton("Iced Latte");
+		menu.add(menu2);
+		JButton menu3 = new JButton("Iced Vanilla Latte");
+		menu.add(menu3);
+		JButton menu4 = new JButton("Iced Mocha");
+		menu.add(menu4);
+		JButton menu5 = new JButton("Hot Americano");
+		menu.add(menu5);
+		JButton menu6 = new JButton("Hot Latte");
+		menu.add(menu6);
+		JButton menu7 = new JButton("Hot Vanilla Latte");
+		menu.add(menu7);
+		JButton menu8 = new JButton("Hot Mocha");
+		menu.add(menu8);
+		
+		
+		JPanel sale = new JPanel();
+		sale.setBounds(600, 30, 600, 500);
+		sale.setBackground(Color.WHITE);
+		panel_2.add(sale);
+		sale.setLayout(null);
+		JLabel salelabel = new JLabel("Sale");
+		salelabel.setFont(new Font("Verdana", Font.BOLD, 20));
+		salelabel.setBounds(300, 6, 108, 29);
+		sale.add(salelabel);
+		JButton charge = new JButton("Charge : ");
+		charge.setAction(action);
+		charge.setFont(new Font("Arial", Font.BOLD, 15));
+		charge.setForeground(new Color(0, 0, 0));
+		charge.setBounds(0,400,600,100);
+		charge.setBackground(new Color(0, 0, 0));//2896CC 이 컬러코드로 하고싶다. 
+		sale.add(charge);
+		
+		
+		JPanel bottom = new JPanel();
+		bottom.setBounds(0, 530, 1200, 100);
+		bottom.setBackground(new Color(0, 0, 51));
+		panel_2.add(bottom);
+		JLabel bottomlabel = new JLabel("bottom");
+		bottom.add(bottomlabel);
+		
+		panel_2.setVisible(true);
+		menu.setVisible(false);
+		sale.setVisible(false);
 
 		logBtn.addActionListener(new ActionListener() {
 
@@ -133,6 +192,8 @@ public class swing {
 				if(id.equals(txtID.getText())&& pw.equals(txtpass.getText())) {
 					JOptionPane.showMessageDialog(null, "Login Success");
 					panel_2.setVisible(true);
+					menu.setVisible(true);
+					sale.setVisible(true);
 					panel_1.setVisible(false);
 					
 				}else {
@@ -145,5 +206,13 @@ public class swing {
 		
 
 		 
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
